51e2661291a645b4410c9f6e56cd24e92d957b4c
kingyinliang <17600813147@163.com>
2021-07-19 17:49:36 +0800

    feat(select): select-system.vue

--- src/project/LOGIN/pages/login.ts
+++ src/project/LOGIN/pages/login.ts
@@ -1,18 +1,29 @@
+import { ref, Ref } from 'vue'
 import SSOLogin from '@/utils/SSOLogin'
 
 interface LoginTs {
+  systemVisible: Ref
+  selectSystem: Ref
   init: () => void
 }
 export default function (): LoginTs {
+  const systemVisible = ref(false)
+  const selectSystem = ref()
   const init = () => {
     SSOLogin.getUserInfo().then(({ data }) => {
       sessionStorage.setItem('userInfo', JSON.stringify(data.data || {}))
-
-      window.location.href = '/qms'
+      if (data.data.defaultFactory && data.data.userFactory.filter((item: any) => item.deptCode === data.data.defaultFactory).length) {
+        const dfFa = data.data.userFactory.filter((item: any) => item.deptCode === data.data.defaultFactory)[0]
+        selectSystem.value.goSystem(dfFa)
+      } else {
+        systemVisible.value = true
+      }
     })
   }
 
   return {
-    init
+    init,
+    selectSystem,
+    systemVisible
   }
 }
