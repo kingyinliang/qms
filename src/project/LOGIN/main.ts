f9ed6847a3eee6ea745da8c3c5fe7b4e15f51f5d
kingyinliang <17600813147@163.com>
2021-09-30 17:29:58 +0800

    fix(bug): fix bug

--- src/project/LOGIN/main.ts
+++ src/project/LOGIN/main.ts
@@ -4,11 +4,15 @@ import router from './router'
 import VueCookies from '@/components/cookie/vue-cookies'
 import ElementPlus from 'element-plus'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
-import 'element-plus/lib/theme-chalk/index.css'
+
+import Mds from '@/components'
 
 import '@/assets/scss/index.scss'
 import '@/assets/icon/iconfont.css'
+import '@/assets/icon/qms-iconfont.css'
+import '@/assets/icon/qms-iconfont.js'
+import 'element-plus/lib/theme-chalk/index.css'
 
 const app = createApp(AppVue)
 
-app.use(router).use(ElementPlus, { locale }).use(VueCookies).mount('#app')
+app.use(router).use(ElementPlus, { locale }).use(Mds).use(VueCookies).mount('#app')
