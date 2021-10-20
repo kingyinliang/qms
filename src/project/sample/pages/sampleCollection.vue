b02f860057a66ba8d75dd0a15ca2fcaf6a0a389a
kingyinliang <17600813147@163.com>
2021-10-20 16:58:39 +0800

    feat(sample): 样品管理

--- src/project/sample/pages/sampleCollection.vue
+++ src/project/sample/pages/sampleCollection.vue
@@ -15,30 +15,30 @@
       <el-table ref="multipleTableRef" border :cell-style="{'text-align':'center'}" :data="tableData" tooltip-effect="dark" @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="45" />
         <el-table-column type="index" fixed="left" label="序号" width="50" />
-        <el-table-column label="样品码" prop="itemCode" :show-overflow-tooltip="true" />
-        <el-table-column label="状态" prop="itemName" min-width="120" :show-overflow-tooltip="true" />
-        <el-table-column label="检验内容" prop="creator" min-width="150" :show-overflow-tooltip="true" />
-        <el-table-column label="送样部门" prop="creator" min-width="150" :show-overflow-tooltip="true" />
-        <el-table-column label="送达时间" prop="creator" min-width="150" :show-overflow-tooltip="true" />
-        <el-table-column label="取样时间" prop="creator" min-width="150" :show-overflow-tooltip="true" />
+        <el-table-column label="样品码" prop="sampleCode" :show-overflow-tooltip="true" />
+        <el-table-column label="状态" prop="taskStatus" min-width="120" :show-overflow-tooltip="true" />
+        <el-table-column label="检验内容" prop="inspectContent" min-width="150" :show-overflow-tooltip="true" />
+        <el-table-column label="送样部门" prop="sendSampleDeptName" min-width="150" :show-overflow-tooltip="true" />
+        <el-table-column label="送达时间" prop="deliveryDate" min-width="150" :show-overflow-tooltip="true" />
+        <el-table-column label="取样时间" prop="sampleEndDate" min-width="150" :show-overflow-tooltip="true" />
       </el-table>
     </mds-card>
   </div>
   <el-dialog v-model="userNameDialog" title="请扫描工牌" width="50%">
-    <el-form size="small" @keyup.enter="query" @submit.prevent>
+    <el-form size="small" @keyup.enter="collectionSubmit" @submit.prevent>
       <el-form-item label="请扫描您的工作证：">
         <el-input ref="userNameRef" v-model="userName" autofocus></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary"><i class="qmsIconfont qms-shouyang" @click="collection"/> 收样</el-button>
+        <el-button type="primary"><i class="qmsIconfont qms-shouyang" @click="collectionSubmit"/> 收样</el-button>
       </el-form-item>
     </el-form>
   </el-dialog>
 </template>
 
 <script lang="ts">
-import { defineComponent, nextTick, onMounted, ref } from 'vue'
-import { SAMPLE_HANDOVER_QUERY } from '@/api/api'
+import { ComponentInternalInstance, defineComponent, getCurrentInstance, nextTick, onMounted, ref } from 'vue'
+import { SAMPLE_HANDOVER_QUERY, SAMPLE_HANDOVER_COLLECTION } from '@/api/api'
 
 interface TableData{
   id?: string
@@ -46,6 +46,9 @@ interface TableData{
 export default defineComponent({
   name: 'sampleCollection',
   setup () {
+    const ctx = getCurrentInstance() as ComponentInternalInstance
+    const proxy = ctx.proxy as any
+
     const sampleCodeRef = ref()
     const multipleTableRef = ref()
     const userNameRef = ref()
@@ -56,20 +59,38 @@ export default defineComponent({
     const tableData = ref<TableData[]>([]) // 表格数据
 
     const query = async () => {
-      const data = {}
-      const res = await SAMPLE_HANDOVER_QUERY({ sampleCode: sampleCode.value })
-      console.log(res)
-      tableData.value.push(data)
-      multipleTableRef.value.toggleRowSelection(data)
+      const { data } = await SAMPLE_HANDOVER_QUERY({ sampleCode: sampleCode.value })
+      console.log(data)
+      if (data.data.taskStatus === '已送达') {
+        tableData.value.push(data.data)
+        multipleTableRef.value.toggleRowSelection(data.data)
+      } else {
+        proxy.$errorToast('该样品码不否存或不是已送达状态')
+      }
     }
     const handleSelectionChange = (val: TableData[]) => {
       multipleSelection.value = val.map((item: TableData) => item)
     }
     const collection = async () => {
-      console.log(1)
-      userNameDialog.value = true
-      await nextTick()
-      userNameRef.value.focus()
+      if (multipleSelection.value.length) {
+        userNameDialog.value = true
+        await nextTick()
+        userNameRef.value.focus()
+      } else {
+        proxy.$warningToast('请选择数据')
+      }
+    }
+    const collectionSubmit = async () => {
+      await SAMPLE_HANDOVER_COLLECTION({
+        taskSamples: multipleSelection.value,
+        taskStatus: '已收样',
+        changer: userName.value
+      })
+      proxy.$successToast('操作成功')
+      userNameDialog.value = false
+      multipleSelection.value.forEach(item => {
+        tableData.value = tableData.value.filter(it => it.id !== item.id)
+      })
     }
 
     onMounted(async () => {
@@ -87,7 +108,8 @@ export default defineComponent({
       userName,
       handleSelectionChange,
       query,
-      collection
+      collection,
+      collectionSubmit
     }
   }
 })
--- src/project/sample/pages/sampleDelivery.vue
+++ src/project/sample/pages/sampleDelivery.vue
@@ -15,19 +15,30 @@
       <el-table ref="multipleTableRef" border :cell-style="{'text-align':'center'}" :data="tableData" tooltip-effect="dark" @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="45" />
         <el-table-column type="index" fixed="left" label="序号" width="50" />
-        <el-table-column label="样品码" prop="itemCode" :show-overflow-tooltip="true" />
-        <el-table-column label="状态" prop="itemName" min-width="120" :show-overflow-tooltip="true" />
-        <el-table-column label="检验内容" prop="creator" min-width="150" :show-overflow-tooltip="true" />
-        <el-table-column label="送样部门" prop="creator" min-width="150" :show-overflow-tooltip="true" />
-        <el-table-column label="取样人" prop="creator" min-width="150" :show-overflow-tooltip="true" />
-        <el-table-column label="取样时间" prop="creator" min-width="150" :show-overflow-tooltip="true" />
+        <el-table-column label="样品码" prop="sampleCode" :show-overflow-tooltip="true" />
+        <el-table-column label="状态" prop="taskStatus" min-width="120" :show-overflow-tooltip="true" />
+        <el-table-column label="检验内容" prop="inspectContent" min-width="150" :show-overflow-tooltip="true" />
+        <el-table-column label="送样部门" prop="sendSampleDeptName" min-width="150" :show-overflow-tooltip="true" />
+        <el-table-column label="取样人" prop="sampler" min-width="150" :show-overflow-tooltip="true" />
+        <el-table-column label="取样时间" prop="sampleEndDate" min-width="150" :show-overflow-tooltip="true" />
       </el-table>
     </mds-card>
   </div>
+  <el-dialog v-model="userNameDialog" title="请扫描工牌" width="50%">
+    <el-form size="small" @keyup.enter="deliverySubmit" @submit.prevent>
+      <el-form-item label="请扫描您的工作证：">
+        <el-input ref="userNameRef" v-model="userName" autofocus></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary"><i class="qmsIconfont qms-shouyang" @click="deliverySubmit"/> 收样</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
 
 <script lang="ts">
-import { defineComponent, nextTick, onMounted, ref } from 'vue'
+import { ComponentInternalInstance, defineComponent, getCurrentInstance, nextTick, onMounted, ref } from 'vue'
+import { SAMPLE_HANDOVER_QUERY, SAMPLE_HANDOVER_DELIVERY } from '@/api/api'
 
 interface TableData{
   id?: string
@@ -35,22 +46,51 @@ interface TableData{
 export default defineComponent({
   name: 'sampleDelivery',
   setup () {
+    const ctx = getCurrentInstance() as ComponentInternalInstance
+    const proxy = ctx.proxy as any
+
     const sampleCodeRef = ref()
     const multipleTableRef = ref()
+    const userNameRef = ref()
+    const userNameDialog = ref(false)
     const sampleCode = ref('')
+    const userName = ref('')
     const multipleSelection = ref<TableData[]>([])
     const tableData = ref<TableData[]>([]) // 表格数据
 
-    const query = () => {
-      const data = {}
-      tableData.value.push(data)
-      multipleTableRef.value.toggleRowSelection(data)
+    const query = async () => {
+      const { data } = await SAMPLE_HANDOVER_QUERY({ sampleCode: sampleCode.value })
+      console.log(data)
+      if (data.data.taskStatus === '取样中') {
+        tableData.value.push(data.data)
+        multipleTableRef.value.toggleRowSelection(data.data)
+      } else {
+        proxy.$errorToast('该样品码不否存或不是已送达状态')
+      }
     }
     const handleSelectionChange = (val: TableData[]) => {
       multipleSelection.value = val.map((item: TableData) => item)
     }
-    const delivery = () => {
-      console.log(1)
+    const delivery = async () => {
+      if (multipleSelection.value.length) {
+        userNameDialog.value = true
+        await nextTick()
+        userNameRef.value.focus()
+      } else {
+        proxy.$warningToast('请选择数据')
+      }
+    }
+    const deliverySubmit = async () => {
+      await SAMPLE_HANDOVER_DELIVERY({
+        taskSamples: multipleSelection.value,
+        taskStatus: '已送达',
+        changer: userName.value
+      })
+      proxy.$successToast('操作成功')
+      userNameDialog.value = false
+      multipleSelection.value.forEach(item => {
+        tableData.value = tableData.value.filter(it => it.id !== item.id)
+      })
     }
 
     onMounted(async () => {
@@ -60,12 +100,16 @@ export default defineComponent({
 
     return {
       sampleCodeRef,
+      userNameRef,
       multipleTableRef,
       sampleCode,
       tableData,
+      userNameDialog,
+      userName,
       handleSelectionChange,
       query,
-      delivery
+      delivery,
+      deliverySubmit
     }
   }
 })
